035b44385350ebeed4f164d3dc1821f05b99e307
magiccane204 <134020332+magiccane204@users.noreply.github.com>
2026-01-05 18:42:23 +0530

    Update Customer.js

--- src/Customer.js
+++ src/Customer.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const cors = require("cors");
-const bodyParser = require("body-parser");
+
 const { MongoClient } = require("mongodb");
 
 const app = express();
 app.use(cors());
-app.use(bodyParser.json());
+
 
 /* ===============================
    MongoDB Connection
@@ -141,5 +141,6 @@ const PORT = 5000;
 app.listen(PORT, () => {
   console.log(`🚀 Server running on http://localhost:${PORT}`);
 });
+
 
 
