a3bc581afeef148cf3caf36c14b65e52deb6c289
magiccane204 <134020332+magiccane204@users.noreply.github.com>
2026-01-05 18:41:59 +0530

    Update Customer.js

--- src/Customer.js
+++ src/Customer.js
@@ -10,7 +10,7 @@ app.use(bodyParser.json());
 /* ===============================
    MongoDB Connection
 ================================ */
-const MONGO_URL = MONGODB_URI;
+const MONGO_URL =process.env.MONGODB_URI;
 const DB_NAME = "crm";
 
 let db, customersCollection;
@@ -141,4 +141,5 @@ const PORT = 5000;
 app.listen(PORT, () => {
   console.log(`🚀 Server running on http://localhost:${PORT}`);
 });
+
 
