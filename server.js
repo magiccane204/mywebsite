433064ac4361063e36b005d85654fdcee8cd2888
magiccane204 <134020332+magiccane204@users.noreply.github.com>
2026-03-15 23:49:02 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1408,7 +1408,8 @@ app.post(
     try {
 
       const task = await Task.findById(req.params.id);
-
+      if(req.user.role==="Employee" && task.EmployeeEmail!==req.user.email){
+      return res.status(403).json({message:"Not your task"});
       if (!task)
         return res.status(404).json({ message: "Task not found" });
 
